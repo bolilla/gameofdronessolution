9870660dc79c6a3ce393512127c13f1cb002e4d5
Borja Roux <borja.roux@gmail.com>
2014-03-15 08:34:15 +0100

    Implemented strategy: each drone to the nearest zone and stay put

--- gameOfDrones.go
+++ gameOfDrones.go
@@ -1,20 +1,93 @@
 //Participating Game of Drones by CodinGame
 package main
 
-import "fmt"
-
-// import "os"
+import (
+	"fmt"
+	"io"
+	"math"
+	"os"
+)
 
 const (
-	DEBUG       = false //True iff traces are activated
-	ZONE_RADIUS = 100   //Radius of the zones
+	DEBUG          = false //True iff traces are activated
+	ZONE_RADIUS    = 100.0 //Radius of the zones
+	DRONE_MOVEMENT = 100.0 //Maximum movement of a drone in a turn
+	MAX_DISTANCE   = 44    //Number of turns to cross the board
 )
 
 var (
-	players []player //all the player of drones. Array index = player's ID
-	whoami  int      //index of my player in the array of players
-	zones   []zone   //all game zones
+	numPlayers         int       //Number of players in the game
+	numZones           int       //Number of zones in the game
+	players            []player  //all the player of drones. Array index = player's ID
+	numDronesPerplayer int       //Number of drones each player has
+	whoami             int       //index of my player in the array of players
+	zones              []zone    //all game zones
+	distances          [][][]int //Distances for each of the players, for each of the drones to each of the zones
 )
+
+//Prints the movements of own drones
+func play() {
+	// Do not move any drone
+	for _, m := range decideMovement() {
+		fmt.Println(m.x, m.y)
+	}
+}
+
+//Returns the movement for each of the drones
+func decideMovement() []point {
+	return stratEachDroneToNearestZone()
+}
+
+//Returns the movements based on the following strategy:
+//- Each drone moves to its nearest zone
+//- [NO] Only a drone per zone
+//- Move to the center of the zone
+func stratEachDroneToNearestZone() []point {
+	result := make([]point, numDronesPerplayer)
+	calculateDistances()
+	//	zonesWithDrone := make(map[int]bool, numZones)
+	for dId := 0; dId < numDronesPerplayer; dId += 1 {
+		minDist := MAX_DISTANCE
+		bestZone := -1
+		for zId := 0; zId < numZones; zId += 1 {
+			//if _, occupied := zonesWithDrone[zId]; !occupied && distances[whoami][dId][zId] <= minDist {
+			if distances[whoami][dId][zId] <= minDist {
+				minDist = distances[whoami][dId][zId]
+				bestZone = zId
+			}
+		}
+		result[dId] = zones[bestZone].pos
+		//zonesWithDrone[bestZone] = true
+	}
+
+	return result
+}
+
+//Calculates the distances from each of my drones to each of the zones' centres
+func calculateDistances() {
+	for pId, _ := range players {
+		for dId, d := range players[pId].drones {
+			for zId, z := range zones {
+				distances[pId][dId][zId] = turnBasedDistance(d, z.pos)
+			}
+		}
+	}
+}
+
+//Calculates the number of turns that it would take to each drone to reach each zone
+func turnBasedDistance(pointA, pointB point) int {
+	euc := euclideanDistance(pointA, pointB)
+	if euc < ZONE_RADIUS {
+		return 0
+	}
+	return int(math.Ceil((euc - (ZONE_RADIUS)) / DRONE_MOVEMENT))
+}
+
+//Returns the euclidean distance between two points
+func euclideanDistance(pointA, pointB point) float64 {
+	return math.Floor(math.Sqrt((float64(pointB.x-pointA.x) * float64(pointB.x-pointA.x)) +
+		(float64(pointB.y-pointA.y) * float64(pointB.y-pointA.y))))
+}
 
 type point struct {
 	x, y int
@@ -35,8 +108,7 @@ func newZone() zone {
 }
 
 //Reads the game initialization information
-func readInit() {
-	var numPlayers, numZones, numDronesPerplayer int
+func readBoard(in io.Reader) {
 	fmt.Scanf("%d %d %d %d\n", &numPlayers, &whoami, &numDronesPerplayer, &numZones)
 	players = make([]player, numPlayers)
 	for i, _ := range players {
@@ -45,22 +117,30 @@ func readInit() {
 	zones = make([]zone, numZones)
 	for i, _ := range zones {
 		zones[i] = newZone()
-		fmt.Scanf("%d %d\n", &zones[i].pos.x, &zones[i].pos.y)
+		fmt.Fscanf(in, "%d %d\n", &zones[i].pos.x, &zones[i].pos.y)
+	}
+	distances = make([][][]int, numPlayers)
+	for pId := 0; pId < numPlayers; pId += 1 {
+		distances[pId] = make([][]int, numDronesPerplayer)
+		for dId := 0; dId < numDronesPerplayer; dId += 1 {
+			distances[pId][dId] = make([]int, numZones)
+		}
 	}
 }
 
 //Reads the information of a turn
-func parseTurn() bool {
+func parseTurn(in io.Reader) bool {
 	for i, _ := range zones {
-		_, err := fmt.Scanf("%d\n", &zones[i].owner)
+		_, err := fmt.Fscanf(in, "%d\n", &zones[i].owner)
 		if err != nil {
 			fmt.Println("Error reading turn zones owners:", err)
 			return false
 		}
 	}
+
 	for i, _ := range players {
 		for j, _ := range players[i].drones {
-			_, err := fmt.Scanf("%d %d\n", &players[i].drones[j].x, &players[i].drones[j].y)
+			_, err := fmt.Fscanf(in, "%d %d\n", &players[i].drones[j].x, &players[i].drones[j].y)
 			if err != nil {
 				fmt.Println("Error reading turn drones:", err)
 				return false
@@ -70,18 +150,11 @@ func parseTurn() bool {
 	return true
 }
 
-//Prints the movements of own drones
-func play() {
-	// Do not move any drone
-	for _, d := range players[whoami].drones {
-		fmt.Println(d.x, d.y)
-	}
-}
-
+//Unleashes the beast
 func main() {
-	readInit()
-	debug("Current status:", players, whoami, zones)
-	for parseTurn() {
+	readBoard(os.Stdin)
+	debug("Initial status:", players, whoami, zones)
+	for parseTurn(os.Stdin) {
 		debug("Current status:", players, whoami, zones)
 		play()
 	}
---
+++ gameOfDrones_test.go
@@ -0,0 +1,87 @@
+// Codingame - Game of Drones
+package main
+
+import (
+	"testing"
+)
+
+const FILE_TESTS_BASE = `C:\Users\borja\programacion\codinggame\GameOfDronesSolution\testInputs`
+
+//Tests method turnBasedDistance
+func TestTurnBasedDistance(t *testing.T) {
+	var testCases = []struct {
+		in1, in2 point
+		out      int
+	}{
+		{point{500, 500}, point{450, 500}, 0}, //up - 0
+		{point{500, 500}, point{550, 500}, 0}, //down - 0
+		{point{500, 500}, point{500, 450}, 0}, //left - 0
+		{point{500, 500}, point{500, 550}, 0}, //right - 0
+
+		{point{500, 500}, point{400, 500}, 0}, //up - 0
+		{point{500, 500}, point{600, 500}, 0}, //down - 0
+		{point{500, 500}, point{500, 400}, 0}, //left - 0
+		{point{500, 500}, point{500, 600}, 0}, //right - 0
+
+		{point{500, 500}, point{399, 500}, 1}, //up - 1
+		{point{500, 500}, point{601, 500}, 1}, //down - 1
+		{point{500, 500}, point{500, 399}, 1}, //left - 1
+		{point{500, 500}, point{500, 601}, 1}, //right - 1
+
+		{point{500, 500}, point{299, 500}, 2}, //up - 2
+		{point{500, 500}, point{701, 500}, 2}, //down - 2
+		{point{500, 500}, point{500, 299}, 2}, //left - 2
+		{point{500, 500}, point{500, 701}, 2}, //right - 2
+
+		{point{500, 500}, point{430, 570}, 0}, //up-righ - 0
+		{point{500, 500}, point{430, 430}, 0}, //up-left - 0
+		{point{500, 500}, point{570, 570}, 0}, //down-right - 0
+		{point{500, 500}, point{570, 430}, 0}, //down-left - 0
+
+		{point{500, 500}, point{428, 572}, 1}, //up-righ - 1
+		{point{500, 500}, point{428, 428}, 1}, //up-left - 1
+		{point{500, 500}, point{572, 572}, 1}, //down-right - 1
+		{point{500, 500}, point{572, 428}, 1}, //down-left - 1
+
+		{point{0, 0}, point{0, 0}, 0}, //border
+
+		{point{500, 500}, point{500, 500}, 0}, //same point
+	}
+	for i, testCase := range testCases {
+		if result := turnBasedDistance(testCase.in1, testCase.in2); testCase.out != result {
+			t.Error("Error in item", i, "Got", result, "Expected", testCase.out, "Case:", testCase)
+		}
+	}
+}
+
+//Tests method euclideanDistance
+func TestEuclideanDistance(t *testing.T) {
+	var testCases = []struct {
+		in1, in2 point
+		out      float64
+	}{
+		{point{500, 500}, point{450, 500}, 50.0}, //up - 0
+		{point{500, 500}, point{550, 500}, 50.0}, //down - 0
+		{point{500, 500}, point{500, 450}, 50.0}, //left - 0
+		{point{500, 500}, point{500, 550}, 50.0}, //right - 0
+
+		{point{500, 500}, point{430, 570}, 98.0}, //up-righ - 0
+		{point{500, 500}, point{430, 430}, 98.0}, //up-left - 0
+		{point{500, 500}, point{570, 570}, 98.0}, //down-right - 0
+		{point{500, 500}, point{570, 430}, 98.0}, //down-left - 0
+
+		{point{500, 500}, point{429, 571}, 100.0}, //up-righ - 1
+		{point{500, 500}, point{429, 429}, 100.0}, //up-left - 1
+		{point{500, 500}, point{571, 571}, 100.0}, //down-right - 1
+		{point{500, 500}, point{571, 429}, 100.0}, //down-left - 1
+
+		{point{0, 0}, point{0, 0}, 0}, //border
+
+		{point{500, 500}, point{500, 500}, 0}, //same point
+	}
+	for i, testCase := range testCases {
+		if result := euclideanDistance(testCase.in1, testCase.in2); testCase.out != result {
+			t.Error("Error in item", i, "Got", result, "Expected", testCase.out, "Case:", testCase)
+		}
+	}
+}
